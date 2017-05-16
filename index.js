c89db3869bb89e71bd191d8376803371bb13aca2
nhanltv <nhanltv.bd@gmail.com>
2017-05-16 16:54:25 +0700

    get list file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /**
  * Created by navnahn on 5/16/17.
  */
+"use strict";
+var rFs = require("fs");
 
 var gBaseDirPath = null;
 
@@ -10,5 +12,24 @@ module.exports = {
     },
     test1: function () {
         return gBaseDirPath;
+    },
+    test2: function () {
+        return getFiles(gBaseDirPath);
     }
+};
+
+var getFiles = function (dir, fileList){
+    fileList = fileList || [];
+
+    var files = fs.readdirSync(dir);
+    for(var i in files){
+        if (!files.hasOwnProperty(i)) continue;
+        var name = dir+'/'+files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, fileList);
+        } else {
+            fileList.push(name);
+        }
+    }
+    return fileList;
 };
